287b6d2f082ed2917a68f48fcf184a4f766e2717
nombrekeff <manoloedge96@gmail.com>
2021-02-10 14:41:55 +0100

    feat(architecture): added AppState and ChangeNotifierProvider

--- lib/Base/View.base.dart
+++ lib/Base/View.base.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/widgets.dart';
+import 'package:rec/Providers/AppState.dart';
 
 abstract class ViewBaseState<T extends StatefulWidget> extends State<T> {
-  Widget buildView(BuildContext context);
+  Widget buildView(BuildContext context, AppState appState);
 
   @override
   Widget build(BuildContext context) {
-    return buildView(context);
+    AppState appState = AppState.of(context);
+    return buildView(context, appState);
   }
 }
--- lib/Views/Login.view.dart
+++ lib/Views/Login.view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:rec/Base/View.base.dart';
+import 'package:rec/Providers/AppState.dart';
 
 class LoginView extends StatefulWidget {
   LoginView({Key key}) : super(key: key);
@@ -12,7 +13,7 @@ class _LoginViewState extends ViewBaseState<LoginView> {
   _LoginViewState() : super();
 
   @override
-  Widget buildView(BuildContext context) {
+  Widget buildView(BuildContext context, AppState appState) {
     return Container();
   }
 }
