7d32d61307bef2897afbe82d5cd3d282ec5eb62c
nombrekeff <manoloedge96@gmail.com>
2021-05-10 13:24:17 +0200

    files: renamed LocationProvider.dart to LocationPermissionProvider.dart

--- lib/Permissions/PermissionProviders.dart
+++ lib/Permissions/PermissionProviders.dart
@@ -1,7 +1,7 @@
 import 'package:rec/Permissions/providers/CameraPermissionProvider.dart';
 import 'package:rec/Permissions/providers/ContactsPermissionProvider.dart';
 import 'package:rec/Permissions/PermissionProvider.dart';
-import 'package:rec/Permissions/providers/LocationProvider.dart';
+import 'package:rec/Permissions/providers/LocationPermissionProvider.dart';
 import 'package:rec/Permissions/providers/QrPermissionProvider.dart';
 
 class PermissionProviders {
