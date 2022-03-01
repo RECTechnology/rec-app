d48e723fd2f3eed23febadb37c20296a1a0964df
Manolo Edge <nombrekeff@rallf.com>
2022-03-01 12:31:11 +0100

    chore(release): v2.1.5+61

--- scripts/prepare-env.sh
+++ scripts/prepare-env.sh
@@ -4,7 +4,7 @@ envPath='lib/Environments/env'
 env=$1
 
 if [ -z "$1" ]; then
-    echo "No environment supplied, allowed: [pre, prod, local]"
+    echo "No environment supplied, allowed: [pre, prod, local, sandbox]"
     exit 1
 fi
 
