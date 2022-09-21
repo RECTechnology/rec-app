31533cad6e3773b10aa2dbe4a92c452aae40bcfe
Manolo Edge <nombrekeff@rallf.com>
2022-09-21 12:43:13 +0200

    fix: added privacy policy to settings

--- lib/Pages/Private/Home/Tabs/Settings/other/other_settings_list.dart
+++ lib/Pages/Private/Home/Tabs/Settings/other/other_settings_list.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:rec/Components/ListTiles/SectionTitleTile.dart';
 import 'package:rec/Components/ListTiles/SettingsListTile.dart';
+import 'package:rec/Pages/Private/Shared/InAppBrowser.dart';
 import 'package:rec/helpers/RecNavigation.dart';
 import 'package:rec/config/routes.dart';
+import 'package:rec/providers/AppLocalizations.dart';
 
 class OtherSettingsList extends StatelessWidget {
   const OtherSettingsList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context);
+
     return Column(
       children: [
         SectionTitleTile('SETTINGS_OTHER'),
@@ -19,6 +23,19 @@ class OtherSettingsList extends StatelessWidget {
             context,
             Routes.settingsHelp,
           ),
+        ),
+        SettingsListTile(
+          title: 'SETTINGS_PRIVACY',
+          icon: Icons.privacy_tip,
+          onTap: () {
+            InAppBrowser.openLink(
+              context,
+              localizations!.translate('link_privacy'),
+              title: localizations.translate(
+                'SETTINGS_PRIVACY',
+              ),
+            );
+          },
         ),
       ],
     );
